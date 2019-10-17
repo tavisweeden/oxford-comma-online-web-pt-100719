0f03723066777c17775819c09c7b0853454bcf5d
Tavis Weeden <tavisweeden@gmail.com>
2019-10-17 01:47:03 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,29 @@
 def oxford_comma(array)
+  
+ if 
+ array.length == 1 
+ array.each do |str| 
+ array << str
+ 
+ end
+ array.join
 
+ elsif
+ 
+ array.length == 2
+ array.each do |str|
+ array << str   
+ 
+ end
+ array.join(" and ")
+ 
+ else
+ 
+ array.length == 3
+ array.each do |str|
+ array << str   
+ 
+  end 
+ array.join("and")  
+ end
 end
