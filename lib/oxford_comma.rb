c6e1ebd04ae7356c45365e26a08c8c94e3e19333
Tavis Weeden <tavisweeden@gmail.com>
2019-10-17 03:01:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
-def oxford_comma(array)
-
+ def oxford_comma(array)
+  
+ if 
+ array.length == 1 
+ array.join
+ 
+ 
+ elsif
+ 
+  array.length == 2
+  array.join(" and ")
+ 
+ else
+ 
+ array.length == 3
+ array[-1] = "and " + array[-1]
+ array.join(", ") 
+ 
+ 
+ end
 end
