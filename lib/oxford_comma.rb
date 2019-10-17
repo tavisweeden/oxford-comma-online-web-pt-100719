4429424360683e5dfe71ed948b0d2a6de284106e
Tavis Weeden <tavisweeden@gmail.com>
2019-10-17 02:09:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,23 +2,17 @@
   
  if 
  array.length == 1 
- #array.each do |str| 
- #array << str
  array.join
  
  
  elsif
  
   array.length == 2
- #array.each do |str|
- #array << str   
- array.join(" and ")
+  array.join(" and ")
  
  else
  
  array.length == 3
- #array.each do |str|
- #array << str   
  array.join(",")  
  
  end
